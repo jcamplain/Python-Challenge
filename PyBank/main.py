7415650c98afa94f1d6b39af8499cb6b3dc12242
JC2 <jcamplain22@gmail.com>
2020-08-10 12:15:16 -0500

    Completed and it works

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,18 +1,43 @@
 import os
 import csv
+file = os.path.join("Resources/budget_data.csv")
+with open(file) as csvfile:
+    csvReader = csv.reader(csvfile)
+    csvHeader = next(csvReader)
+    
+    totalMonths = []
+    TprofitLosses = []
+    changeProfit = []
+    
+    for row in csvReader:
+        totalMonths.append(row[0])
+        TprofitLosses.append(int(row[1]))
+    for i in range(len(TprofitLosses)-1):
+        changeProfit.append(TprofitLosses[i+1]-TprofitLosses[i])
+                      
+increase = max(changeProfit)
+decrease = min(changeProfit)
 
-budgetData = os.path.join("Resources/budget_data.csv")
-print(budgetData)
-
-with open(budgetData) as csvFile:
-    reader = csv.reader(csvFile)
-#print(reader)
-    for row in reader:
-        print(row)
-
-#TotalMonths = 0
-
-#for row in reader:
-    #totalMonths = totalMonths + 1
+monthIncrease = changeProfit.index(max(changeProfit))+1
+monthDecrease = changeProfit.index(min(changeProfit))+1
 
 
+print("Financial Analysis")
+print("------------------------")
+print(f"Total Months:{len(totalMonths)}")
+print(f"Total: ${sum(TprofitLosses)}")
+print(f"Average Change: {round(sum(changeProfit)/len(changeProfit),2)}")
+print(f"Greatest Increase in Profits: {totalMonths[monthIncrease]} (${(str(increase))})")
+print(f"Greatest Decrease in Profits: {totalMonths[monthDecrease]} (${(str(decrease))})")      
+
+output = open("output.txt", "w")
+
+line1 = "Financial Analysis"
+line2 = "---------------------"
+line3 = str(f"Total Months: {len(totalMonths)}")
+line4 = str(f"Total: ${sum(TprofitLosses)}")
+line5 = str(f"Average Change: {round(sum(changeProfit)/len(changeProfit),2)}")
+line6 = str(f"Greatest Increase in Profits: {totalMonths[monthIncrease]} (${(str(increase))})")
+line7 = str(f"Greatest Decrease in Profits: {totalMonths[monthDecrease]} (${(str(decrease))})")
+#\n to write on a new line
+output.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1,line2,line3,line4,line5,line6,line7))
